8d62933595fff33eb311c2d52115ae1df401a479
alexespina <85351990+alexespina@users.noreply.github.com>
2021-06-24 20:08:51 -0300

    Add files via upload
    Declaring Number Data Types

---
+++ day2/main_number_data_type.js
@@ -0,0 +1,10 @@
+let idade = 35
+const altura = 9.81  // we use const for non-changing values, gravitational constant in  m/s2
+let peso = 72         // mass in Kilogram
+const imc = 3.14       // pi a geometrical constant
+
+// More Examples
+const temperatura = 100 // temperature in oC, boiling point of water which is a constant
+const bodyTemp = 37      // oC average human body temperature, which is a constant
+
+console.log(idade, altura, peso, imc, temperatura, bodyTemp)
