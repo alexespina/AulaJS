2ec3be02332c37fd19adff7869410ad4ef267346
alexespina <85351990+alexespina@users.noreply.github.com>
2021-06-24 19:54:03 -0300

    Add files via upload
    -- Array

---
+++ day2/main_array.js
@@ -0,0 +1,18 @@
+/*let nums = [1, 2, 3]
+let numbers = [1, 2, 3]
+*/
+//console.log(nums == numbers)  // false
+
+let userOne = {
+name:'Asabeneh',
+role:'teaching',
+country:'Finland'
+}
+
+let userTwo = {
+name:'Asabeneh',
+role:'teaching',
+country:'Finland'
+}
+
+console.log(userOne == userTwo) // false
