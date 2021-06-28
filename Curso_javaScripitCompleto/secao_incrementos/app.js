50f9c0ca05ebd992e9ccd110eb76baca56c78ec3
alexespina <85351990+alexespina@users.noreply.github.com>
2021-06-28 19:46:11 -0300

    Add files via upload

---
+++ Curso_javaScripitCompleto/secao_incrementos/app.js
@@ -0,0 +1,18 @@
+/*let numDeProdutos = 2;
+let totalDeProdutos = numDeProdutos++; //usando o incremento
+console.log(numDeProdutos); //2
+console.log(totalDeProdutos); //3
+*/
+
+/*
+let numDeProdutos =2;  //3
+let totalDeProdutos = ++numDeProdutos;//3
+
+console.log(totalDeProdutos);
+console.log(numDeProdutos);
+*/
+
+let numDeProdutos = 3
+let totalDeProdutos = --numDeProdutos ;
+console.log=(totalDeProdutos);//2
+console.log=(numDeProdutos)//3
