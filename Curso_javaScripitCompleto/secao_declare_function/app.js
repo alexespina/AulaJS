ccff6743501b6291dcafb2025e6677b2a65f4da7
alexespina <85351990+alexespina@users.noreply.github.com>
2021-06-28 19:40:26 -0300

    Add files via upload

---
+++ Curso_javaScripitCompleto/secao_declare_function/app.js
@@ -0,0 +1,10 @@
+console.log(calculaMedia(7,8,9));
+console.log(calculaMedia(5,5,5));
+console.log(calculaMedia(4,5,6));
+console.log(calculaMedia(10,20,30));
+
+function calculaMedia(num1,num2,num3){
+   let media = (num1+num2+num3)/3;
+   return media;
+}
+
