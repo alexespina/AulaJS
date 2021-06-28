ce0230d72a804b75602d3e38c3ace249bf6ee596
alexespina <85351990+alexespina@users.noreply.github.com>
2021-06-28 19:48:33 -0300

    Add files via upload

---
+++ Curso_javaScripitCompleto/secao_while/script.js
@@ -0,0 +1,10 @@
+/**
+ * Trabalhando com o While
+ */
+let soma = 0;
+let numero;
+while(soma < 100){
+   numero = parseInt(prompt("Insira um numero"));
+   soma = soma + numero;
+   console.log(soma);
+}
