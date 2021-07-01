6254c659c42c8325a75bdc2bce240b10dd7fbfd3
alexespina <85351990+alexespina@users.noreply.github.com>
2021-07-01 01:02:43 -0300

    Delete main_number.js

--- main_number.js
+++
@@ -1,7 +0,0 @@
-// Declaring variables with number values
-//let age = 100 // age in years
-const gravity = 9.81 // earth gravity  in m/s2
-const boilingPoint = 100 // water boiling point, temperature in oC
-const PI = 3.14 // geometrical constant
-
-console.log(gravity, boilingPoint, PI)
