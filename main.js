dea05ce119c8dd4314fc6c0a345213605821d44f
alexespina <85351990+alexespina@users.noreply.github.com>
2021-07-01 01:02:58 -0300

    Delete main.js

--- main.js
+++
@@ -1,8 +0,0 @@
-// Declaring different variables of different data types
-let firstName = 'Asabeneh' // first name of a person
-let lastName = 'Yetayeh' // last name of a person
-let country = 'Finland' // country
-let city = 'Helsinki' // capital city
-let age = 100 // age in years
-let isMarried = true
-console.log(firstName, lastName, country, city, age, isMarried)
